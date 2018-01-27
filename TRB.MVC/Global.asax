80dc2319246e3927588d75aa1cf5a7bbbc9f843a
Aganur <aganurych@gmail.com>
2018-01-27 12:02:44 +0200

    Add project files.

---
+++ TRB.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TRB.MVC.MvcApplication" Language="C#" %>
---
+++ TRB.MVC/Scripts/custom/common.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    headerScrolling();
+});
+
+function headerScrolling() {
+    var header = $('.header-wrapper'),
+        headerHeight = $('.header-wrapper').outerHeight(true);
+    $(header).parent().css('padding-top', headerHeight);
+    $(window).scroll(function () {
+        var scrollOffset = $(window).scrollTop();
+        if (scrollOffset < headerHeight) {
+            $(header).css('height', (headerHeight - scrollOffset));
+        }
+        if (scrollOffset > (headerHeight - 215)) {
+            header.addClass('fixme');
+        } else {
+            header.removeClass('fixme');
+        };
+    });
+}
