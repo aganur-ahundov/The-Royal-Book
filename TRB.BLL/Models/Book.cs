6d9b711d8f74052b9bd7ba8127c37c81c47ae403
Aganur <aganurych@gmail.com>
2018-02-19 23:43:50 +0200

    Added DbHelper Added BooksProvider Changed Index design

---
+++ TRB.BLL/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+
+namespace TRB.BLL.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Year { get; set; }
+        public string Description { get; set; }
+
+        public Book() { }
+
+        public Book(DataRow dr)
+        {
+            BookId = Convert.ToInt32(dr["BookId"]);
+            Name = Convert.ToString(dr["BookName"]);
+            Author = Convert.ToString(dr["BookAuthor"]);
+            Year = Convert.ToString(dr["BookYear"]);
+            Description = Convert.ToString(dr["BookDescription"]);
+        }
+    }
+}
---
+++ TRB.BLL/Providers/BooksProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRB.BLL.Helpers;
+using TRB.BLL.Models;
+
+namespace TRB.BLL.Providers
+{
+    public static class BooksProvider
+    {
+        //public static List<Book> GetBooks()
+        //{
+        //    var result = new List<Book>();
+        //    var dt = DBHelper.ExecuteScalar("Proc_Books_Select", CommandType.StoredProcedure);
+
+        //    if (dt != null && dt.Rows.Count > 0)
+        //    {
+        //        result.Add(dt.Rows.OfType<DataRow>().Select(row => new Book(row)));
+        //        foreach (var item in dt.Rows)
+        //        {
+        //            new Book(item));
+        //        }
+        //    }
+        //}
+    }
+}
