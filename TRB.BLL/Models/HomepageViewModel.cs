690a2d59b5b3eb63e59bdc1128b19a15fe20fa01
Aganur <aganurych@gmail.com>
2018-02-21 09:11:00 +0200

    Added category model

---
+++ TRB.BLL/Models/HomepageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TRB.BLL.Models
+{
+    public class HomepageViewModel
+    {
+        public List<Book> Books { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
---
+++ TRB.BLL/Providers/CategoriesProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using TRB.BLL.Helpers;
+using TRB.BLL.Models;
+
+namespace TRB.BLL.Providers
+{
+    public static class CategoriesProvider
+    {
+        public static List<Category> GetCategories()
+        {
+            var result = new List<Category>();
+            var dt = DBHelper.ExecuteScalar("Proc_Category_Select", CommandType.StoredProcedure);
+
+            if (dt != null)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    result.Add(new Category(row));
+                }
+            }
+
+            return result;
+        }
+    }
+}
